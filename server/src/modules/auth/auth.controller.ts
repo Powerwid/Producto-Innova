8664b60f2c64e44c0a5288af4f0195571e1c9436
Joseph Kleyn Mamani Perez <kleyncito@live.com>
2025-12-11 14:05:51 -0500

    fix: se corrigio permisos del guard a vias publicas

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Post, Body } from "@nestjs/common";
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { Public } from "./decorators/public.decorator";
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-
+  constructor(private readonly authService: AuthService) { }
+  @Public()
   @Post('login')
   login(@Body() LoginDto: LoginDto) {
     return this.authService.login(LoginDto);
--- server/src/modules/usuario/usuario.controller.ts
+++ server/src/modules/usuario/usuario.controller.ts
@@ -2,31 +2,37 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsuarioService } from './usuario.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
-
+import { Public } from '../auth/decorators/public.decorator';
+import { Roles } from '../auth/decorators/roles.decorator';
 @Controller('usuario')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
 
+  @Public()
   @Post()
   create(@Body() createUsuarioDto: CreateUsuarioDto) {
     return this.usuarioService.create(createUsuarioDto);
   }
 
+  @Public()
   @Get()
   findAll() {
     return this.usuarioService.findAll();
   }
 
+  @Roles(1, 2)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usuarioService.findOne(+id);
   }
 
+  @Roles(1, 2)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
     return this.usuarioService.update(+id, updateUsuarioDto);
   }
 
+  @Roles(1, 2)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usuarioService.remove(+id);
